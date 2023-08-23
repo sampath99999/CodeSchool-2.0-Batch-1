46479802952f863548ccc131cd9abcf9a0b3093c
vineeth varadi <vineethvaradi@vineeths-MacBook-Pro.local>
2023-08-23 17:03:46 +0530

    task 7

--- Task 5 - Jquery/Vineeth/javascript.js
+++ Task 5 - Jquery/Vineeth/javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     fetchImages();
     fetchAndDisplayData('fashion', '#card-1', 'FASHION');
     fetchAndDisplayData('lifestyle', '#card-2', 'LIFESTYLE');
@@ -6,14 +6,14 @@ $(document).ready(function() {
     fetchAndDisplayData('apple', '#card-5', 'APPLE');
 });
 
-function fetchImages(containerid,num) {
+function fetchImages(containerid, num) {
     $.ajax({
         url: "https://newsapi.org/v2/everything?q=apple&from=2023-08-06&to=2023-08-06&sortBy=popularity&apiKey=cbff5be8dd29429389b689ba16df1ecc",
         method: 'GET',
         success: function (response) {
             const data = response.articles;
-            const data2=response.articles.source;
-            displayImages(data,containerid,num);
+            const data2 = response.articles.source;
+            displayImages(data, containerid, num);
             third(data);
             popular(data);
             displayArticles(data);
@@ -24,7 +24,7 @@ function fetchImages(containerid,num) {
 }
 function displayImages(articles, containerId, i) {
     var imageContainer = $('#' + containerId);
-    
+
     if (articles.length > i) {
         var card = `<div class="card text-bg-dark">
                         <img src="${articles[i].urlToImage}" class="card-img" alt="...">
@@ -34,14 +34,14 @@ function displayImages(articles, containerId, i) {
                             <small>${articles[i].publishedAt}</small>
                         </div>
                     </div>`;
-        
+
         imageContainer.append(card);
     }
 }
 
-fetchImages('image-container',0);
-fetchImages('image-container1',1);
-fetchImages('image-container2',2);
+fetchImages('image-container', 0);
+fetchImages('image-container1', 1);
+fetchImages('image-container2', 2);
 function fetchAndDisplayData(category, containerId, categoryName) {
     $.ajax({
         url: `https://newsapi.org/v2/everything?q=${category}&from=2023-08-07&to=2023-08-07&sortBy=popularity&apiKey=cbff5be8dd29429389b689ba16df1ecc`,
@@ -88,7 +88,7 @@ function generateSmallCards(articles) {
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <small class="card-title">${articles[i].title.slice(0,30)+"..."}</small>
+                        <small class="card-title">${articles[i].title.slice(0, 30) + "..."}</small>
                     </div>
                 </div>
             </div>
@@ -133,7 +133,7 @@ function generateFeaturedCards(data) {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <small class="card-title">${data[i].title.slice(0,20)+"..."}</small>
+                            <small class="card-title">${data[i].title.slice(0, 20) + "..."}</small>
                         </div>
                     </div>
                 </div>
@@ -152,7 +152,7 @@ function generateFeaturedCards2(data) {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <small class="card-title">${data[i].title.slice(0,20)+"..."}</small>
+                            <small class="card-title">${data[i].title.slice(0, 20) + "..."}</small>
                         </div>
                     </div>
                 </div>
@@ -164,7 +164,7 @@ var Articles = $('#l-articles');
 
 function displayArticles(data) {
     let articleContent = '';
-    let combinedCardContent = ''; 
+    let combinedCardContent = '';
 
     for (var i = 0; i < Math.min(data.length, 10); i++) {
         articleContent = `
