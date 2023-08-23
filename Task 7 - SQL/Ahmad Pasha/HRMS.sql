dce7212deb2562df30fea33959c347d47f23352e
Ahmadpasha88 <shaikahamad163@gmail.com>
2023-08-23 18:42:28 +0530

    task completed

--- Task 7 - SQL/Ahmad Pasha/HRMS.sql
+++ Task 7 - SQL/Ahmad Pasha/HRMS.sql
@@ -25,33 +25,13 @@ VALUES (
         '9201377450',
         'male',
         '10-12-2020'
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date
-    )
-VALUES (
+    ), (
         'Ravi',
         'ravi123@gmail.com',
         '9201677450',
         'male',
         '10-11-2020'
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date
-    )
-VALUES (
+    ), (
         'Jaithri',
         'jaithri123@gmail.com',
         '9201377455',
@@ -75,144 +55,56 @@ VALUES (
         'male',
         '10-6-2022',
         1
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Sai',
         'sai123@gmail.com',
         '9202377450',
         'male',
         '10-2-2022',
         1
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'goldy',
         'goldy123@gmail.com',
         '8201377450',
         'female',
         '9-3-2022',
         1
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Sudheer',
         'sudheer123@gmail.com',
         '9231377450',
         'male',
         '10-4-2022',
         2
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Rashmi',
         'rashmi123@gmail.com',
         '9201379450',
         'male',
         '30-5-2022',
         2
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Divya',
         'divya123@gmail.com',
         '9207377450',
         'female',
         '20-6-2022',
         2
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Manjula',
         'manjula123@gmail.com',
         '9207307450',
         'female',
         '10-12-2022',
         3
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Varun',
         'varun123@gmail.com',
         '9201377750',
         'male',
         '10-2-2023',
         3
-    );
-
-INSERT INTO
-    Employees(
-        emp_name,
-        emp_email,
-        emp_phone,
-        emp_gender,
-        hire_date,
-        reportingTo
-    )
-VALUES (
+    ), (
         'Akhila',
         'akhila123@gmail.com',
         '9901377450',
@@ -221,23 +113,7 @@ VALUES (
         3
     );
 
-SELECT
-    e.emp_name,
-    salary,
-    m.emp_name as manager_name
-from employees e
-    join employees m on e.reportingto = m.employee_id
-    INNER join payroll on e.employee_id = payroll.employee_id;
-
 DROP Table employees;
-
--- SELECT reporting_manager_name,reporting_manager_email,emp_firstName, emp_lastName,emp_email from reportingmanagers
-
--- INNER join employees on reportingManagers.reporting_manager_id = employees.reporting_manager_id;
-
--- SELECT emp_salary,reporting_manager_salary from reportingmanagers
-
--- INNER join employees on reportingManagers.reporting_manager_id = employees.reporting_manager_id;
 
 CREATE TABLE
     Department(
@@ -255,97 +131,7 @@ INSERT INTO
         department_name,
         employee_id
     )
-VALUES ('Manager', 'Development', 1);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Manager', 'Testing', 2);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Manager', 'Finance', 3);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Developer', 'Development', 4);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Developer', 'Development', 5);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Developer', 'Development', 6);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Tester', 'Testing', 7);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Tester', 'Testing', 8);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Tester', 'Testing', 9);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Accountant', 'Finance', 10);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Accountant', 'Finance', 11);
-
-INSERT INTO
-    department(
-        job_title,
-        department_name,
-        employee_id
-    )
-VALUES ('Accountant', 'Finance', 12);
-
-SELECT * from department;
+VALUES ('Manager', 'Development', 1), ('Manager', 'Testing', 2), ('Manager', 'Finance', 3), ('Developer', 'Development', 4), ('Developer', 'Development', 5), ('Developer', 'Development', 6), ('Tester', 'Testing', 7), ('Tester', 'Testing', 8), ('Tester', 'Testing', 9), ('Accountant', 'Finance', 10), ('Accountant', 'Finance', 11), ('Accountant', 'Finance', 12);
 
 CREATE TABLE
     Attendance(
@@ -367,125 +153,17 @@ GROUP BY employee_id;
 
 INSERT INTO
     Attendance(employee_id, date, attendance)
-VALUES(1, '21-8-2023', 1);
+VALUES (1, '21-8-2023', 1), (2, '21-8-2023', 1), (3, '21-8-2023', 1), (4, '21-8-2023', 1), (5, '21-8-2023', 1), (6, '21-8-2023', 1), (7, '21-8-2023', 1), (8, '21-8-2023', 1), (9, '21-8-2023', 1), (10, '21-8-2023', 1), (11, '21-8-2023', 1), (12, '21-8-2023', 1);
 
 INSERT INTO
     Attendance(employee_id, date, attendance)
-VALUES(2, '21-8-2023', 1);
+VALUES (1, '22-8-2023', 1), (2, '22-8-2023', 1), (3, '22-8-2023', 1), (4, '22-8-2023', 0), (5, '22-8-2023', 1), (6, '22-8-2023', 1), (7, '22-8-2023', 1), (8, '22-8-2023', 0), (9, '22-8-2023', 1), (10, '22-8-2023', 1), (11, '22-8-2023', 1), (12, '22-8-2023', 0);
 
 INSERT INTO
     Attendance(employee_id, date, attendance)
-VALUES(3, '21-8-2023', 1);
+VALUES (1, '23-8-2023', 1), (2, '23-8-2023', 1), (3, '23-8-2023', 1), (4, '23-8-2023', 1), (5, '23-8-2023', 0), (6, '23-8-2023', 1), (7, '23-8-2023', 0), (8, '23-8-2023', 1), (9, '23-8-2023', 0), (10, '23-8-2023', 1), (11, '23-8-2023', 0), (12, '23-8-2023', 1);
 
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(4, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(5, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(6, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(7, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(8, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(9, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(10, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(11, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(12, '21-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(1, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(2, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(3, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(4, '22-8-2023', 0);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(5, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(6, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(7, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(8, '22-8-2023', 0);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(9, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(10, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(11, '22-8-2023', 1);
-
-INSERT INTO
-    Attendance(employee_id, date, attendance)
-VALUES(12, '22-8-2023', 0);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(1,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(2,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(3,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(4,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(5,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(6,0);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(7,0);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(8,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(9,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(10,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(11,1);
-
-INSERT INTO Attendance(employee_id,attendance) VALUES(12,0);
-
-DELETE FROM attendance WHERE attendance_id=4;
+;
 
 SELECT * from attendance;
 
@@ -495,32 +173,17 @@ CREATE TABLE
     Payroll(
         payroll_id SERIAL PRIMARY KEY,
         employee_id INT REFERENCES Employees(employee_id) NOT NULL,
-        salary INT NOT NULL
+        salary INT NOT NULL,
+        daily_payOut DECIMAL(10, 2)
     );
 
-INSERT INTO Payroll(employee_id,salary) VALUES (1,80000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (2,60000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (3,70000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (4,40000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (5,40000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (6,40000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (7,25000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (8,25000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (9,25000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (10,20000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (11,20000);
-
-INSERT INTO Payroll(employee_id,salary) VALUES (12,20000);
+INSERT INTO
+    Payroll(
+        employee_id,
+        salary,
+        daily_payOut
+    )
+VALUES (1, 80000, 3636.36), (2, 60000, 2727.27), (3, 70000, 3181.81), (4, 40000, 1818.18), (5, 40000, 1818.18), (6, 40000, 1818.18), (7, 25000, 1136.36), (8, 25000, 1136.36), (9, 25000, 1136.36), (10, 20000, 909.09), (11, 20000, 909.09), (12, 20000, 909.09);
 
 DROP Table payroll;
 
@@ -555,198 +218,77 @@ VALUES (
         10000,
         '21-10-2017',
         '12-6-2025'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         2,
         500000,
         100000,
         5000,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         3,
         500000,
         100000,
         10000,
         '21-10-2018',
         '12-6-2024'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         4,
         500000,
         100000,
         5000,
         '21-8-2018',
         '12-6-2025'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         5,
         500000,
         100000,
         5000,
         '21-10-2016',
         '12-6-2024'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         6,
         500000,
         100000,
         5000,
         '21-10-2019',
         '12-6-2027'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         7,
         500000,
         100000,
         5000,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         8,
         500000,
         100000,
         5000,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         9,
         700000,
         100000,
         7000,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         10,
         400000,
         100000,
         4000,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         11,
         300000,
         200000,
         2500,
         '21-10-2017',
         '12-6-2026'
-    );
-
-INSERT INTO
-    loans(
-        employee_id,
-        loan_amount,
-        remaining_amount,
-        deduction_amount,
-        start_date,
-        end_date
-    )
-VALUES (
+    ), (
         12,
         300000,
         100000,
@@ -759,6 +301,16 @@ DROP TABLE loans;
 
 SELECT * FROM loans;
 
+-- get employee table
+
+SELECT * FROM employees;
+
+-- get department table
+
+SELECT * from department;
+
+-- get employee name ,employee email, manager name, manager email, employee salary, manager salary
+
 SELECT
     E.emp_name AS "Employee Name",
     E.emp_email AS "Employee Email",
@@ -770,3 +322,63 @@ FROM employees E
     JOIN employees M ON E.reportingTo = M.employee_id
     LEFT JOIN payroll P_emp ON E.employee_id = P_emp.employee_id
     LEFT JOIN payroll P_mgr ON M.employee_id = P_mgr.employee_id;
+
+-- get employee name and employee loan amount
+
+SELECT emp_name, loan_amount
+from employees e
+    INNER JOIN loans l on e.employee_id = l.employee_id;
+
+-- get highest paid employee name
+
+SELECT emp_name
+from employees e
+    INNER JOIN payroll p on e.employee_id = p.employee_id
+ORDER BY salary DESC
+LIMIT 1;
+
+-- get employee name, employee email, manager name, manager email
+
+SELECT
+    E.emp_name as "Employee Name",
+    E.emp_email AS "Employee Mail",
+    M.emp_name AS "Manager Name",
+    M.emp_email AS "Manager Mail"
+FROM employees E
+    JOIN employees M on E.reportingto = M.employee_id;
+
+-- get emp salary and manager salary as output.
+
+SELECT
+    p_emp.salary as "Employee Salary",
+    p_mrg.salary AS "Manager Salary"
+FROM employees E
+    JOIN employees M on E.reportingto = M.employee_id
+    LEFT JOIN payroll p_emp on p_emp.employee_id = e.employee_id
+    LEFT JOIN payroll p_mrg on p_mrg.employee_id = e.employee_id;
+
+--  get employess with their daily payout
+
+SELECT
+    emp_name,
+    daily_payout - (
+        SELECT
+            deduction_amount
+        from loans
+        WHERE
+            employees.employee_id = loans.employee_id
+    ) / 22
+FROM employees
+    INNER JOIN payroll ON payroll.employee_id = employees.employee_id;
+
+-- get employee name and take home salary including loan deduction amount
+
+SELECT emp_name, salary - (
+        SELECT
+            deduction_amount
+        from loans
+        WHERE
+            employees.employee_id = loans.employee_id
+    ) AS "Take Home Salary"
+FROM employees
+    INNER JOIN payroll ON payroll.employee_id = employees.employee_id;
